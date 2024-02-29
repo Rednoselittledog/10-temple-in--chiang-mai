b0c6f14acf2726a290a4dbda36c58e3a0413d0b5
Aquamate <kanin.mate@gmial.com>
2024-02-29 16:07:05 +0700

    commit23

--- components/TemplePage2.tsx
+++ components/TemplePage2.tsx
@@ -1,7 +1,9 @@
+'use client'
 import { TemplePageProps } from "@/types";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const TemplePage2 = ({
   num,
@@ -11,8 +13,15 @@ const TemplePage2 = ({
   description,
   url,
 }: TemplePageProps) => {
+  const [isPortrait, setIsPortrait] = useState(false);
+
+  useEffect(() => {
+    setIsPortrait(window.matchMedia("(orientation: portrait)").matches)
+    console.log('portrait : ' + isPortrait)
+  }, [window.matchMedia("(orientation: portrait)")]);
+
   return (
-    <div className="page flex flex-col items-center" id={name}>
+    <div className={`page flex flex-col items-center ${isPortrait?'bg-red-500':'bg-blue-500'}`} id={name}>
       <div className="h-full px-10 pt-16 flex flex-col items-center">
 
         <div className="title-container ">
@@ -39,19 +48,19 @@ const TemplePage2 = ({
             <div className="map h-full md:pb-12 pb-5 ">
               {/* <Link href={url}> */}
                 <div className="image-container h-full relative ">
-                  <Image
+                  {/* <Image
                   src="/Images/map-bg.png"
                   layout="fill"
                   objectFit="cover"
                   alt="Your Image"
                   className="map-img"
-                />
-                </div>
-                {/* <img
+                /> */}
+                <img
                   src="/Images/map-bg.png"
                   alt="map-bg"
-                  className=""
-                /> */}
+                  className="md:map-img-2 map-img"
+                />
+                </div>
                 
                 <div className="map-button text-nowrap">
                   <p className="font-medium text-white m-auto">Open in Google Map</p>
