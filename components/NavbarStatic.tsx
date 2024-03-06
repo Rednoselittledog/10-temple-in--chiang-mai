f6fa2a10c73defa714e74cf362784d7e6ae7fc23
Aquamate <kanin.mate@gmial.com>
2024-03-06 19:19:43 +0700

    commit40

--- components/NavbarStatic.tsx
+++ components/NavbarStatic.tsx
@@ -6,21 +6,21 @@ const NavbarStatic = ({ state }: NavbarStaticProps) => {
     return (
         // px-10 2xl:px-16
         <div className="flex flex-row justify-center items-center w-full h-full ">
-            <div  className=" w-full h-full grid grid-cols-5 justify-between items-center sm:overflow-x-scroll">
-            <Link href={'/'}  style={{ textDecoration: 'none' }} className={`px-1 w-full h-full flex justify-center ${state==1?'nav-focus' : 'nav-normal'}`}>
-              <p className={`text-xl my-auto text-title `}>Temples</p>
+            <div  className="w-full h-full lg:grid lg:grid-cols-5 justify-between items-center flex flex-row">
+            <Link href={'/'}  style={{ textDecoration: 'none' }} className={`lg:px-0 px-3 w-full h-full flex justify-center ${state==1?'nav-focus' : 'nav-normal'}`}>
+              <p className={`text-sm md:text-xl my-auto text-title `}>Temples</p>
             </Link>
-            <Link href={'/Transportation'}  style={{ textDecoration: 'none' }}  className={`px-1 w-full h-full flex justify-center ${state==2?'nav-focus' : 'nav-normal'}`}>
-              <p className={`text-xl my-auto text-title `}>Transportation</p>
+            <Link href={'/Transportation'}  style={{ textDecoration: 'none' }}  className={`lg:px-0 px-3 w-full h-full flex justify-center ${state==2?'nav-focus' : 'nav-normal'}`}>
+              <p className={`text-sm md:text-xl my-auto text-title `}>Transportation</p>
             </Link>
-            <Link href={'/Activities'}  style={{ textDecoration: 'none' }}  className={`px-1 w-full h-full flex justify-center ${state==3?'nav-focus' : 'nav-normal'}`}>
-              <p className={`text-xl my-auto text-title `}>Activities</p>
+            <Link href={'/Activities'}  style={{ textDecoration: 'none' }}  className={`lg:px-0 px-3 w-full h-full flex justify-center ${state==3?'nav-focus' : 'nav-normal'}`}>
+              <p className={`text-sm md:text-xl my-auto text-title `}>Activities</p>
             </Link>
-            <Link href={'/Accommodations'}  style={{ textDecoration: 'none' }}  className={`px-1 w-full h-full flex justify-center ${state==4?'nav-focus' : 'nav-normal'}`}>
-              <p className={`text-xl my-auto text-title `}>Accommodations</p>
+            <Link href={'/Accommodations'}  style={{ textDecoration: 'none' }}  className={`lg:px-0 px-3 w-full h-full flex justify-center ${state==4?'nav-focus' : 'nav-normal'}`}>
+              <p className={`text-sm md:text-xl my-auto text-title `}>Accommodations</p>
             </Link>
-            <Link href={'/About us'}  style={{ textDecoration: 'none' }}  className={`px-1 w-full h-full flex justify-center ${state==5?'nav-focus' : 'nav-normal'}`}>
-              <p className={`text-xl my-auto text-title `}>About us</p>
+            <Link href={'/About us'}  style={{ textDecoration: 'none' }}  className={`lg:px-0 px-3 w-full h-full flex justify-center ${state==5?'nav-focus' : 'nav-normal'}`}>
+              <p className={`text-nowrap text-sm md:text-xl my-auto text-title `}>About us</p>
             </Link>
           </div>
         </div>
